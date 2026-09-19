637a485e020ac03a8eee6a9a6df829e6d818bc9a
mahak1808 <mchhabra1808@gmail.com>
2026-09-20 02:03:41 +0530

    feat: update base path in Vite configuration for production deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/production-control-dashboard/",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "./src"),
